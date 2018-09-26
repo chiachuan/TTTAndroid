f6530895512390e08dc181ce1beba02c35190ea5
chiachuan <wu.chiachuan@gmail.com>
2018-09-26 10:12:26 +0800

    Add bouce ball feature

--- app/src/main/java/com/anup/tictactoe/Board.java
+++ app/src/main/java/com/anup/tictactoe/Board.java
@@ -15,8 +15,17 @@ import android.media.AudioManager;
 import android.media.AudioAttributes;
 import android.os.Build;
 
+import android.util.Log;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.TranslateAnimation;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.ImageView;
+
 public class Board extends AppCompatActivity {
 
+    private static final String TAG = "AnimationStarter";
     // Start sound pool//
     private SoundPool soundPool;
 
@@ -46,6 +55,55 @@ public class Board extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+
+        //ball start//
+        Button bounceBallButton = (Button) findViewById(R.id.bounceBallButton);
+        final ImageView bounceBallImage = (ImageView) findViewById(R.id.bounceBallImage);
+
+        bounceBallButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                bounceBallImage.clearAnimation();
+                TranslateAnimation transAnim = new TranslateAnimation(0, 0, 0,
+                        getDisplayHeight()/2);
+                transAnim.setStartOffset(500);
+                transAnim.setDuration(3000);
+                transAnim.setFillAfter(true);
+                transAnim.setInterpolator(new BounceInterpolator());
+                transAnim.setAnimationListener(new AnimationListener() {
+
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        Log.i(TAG, "Starting button dropdown animation");
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                        // TODO Auto-generated method stub
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        Log.i(TAG,
+                                "Ending button dropdown animation. Clearing animation and setting layout");
+                        bounceBallImage.clearAnimation();
+                        final int left = bounceBallImage.getLeft();
+                        final int top = bounceBallImage.getTop();
+                        final int right = bounceBallImage.getRight();
+                        final int bottom = bounceBallImage.getBottom();
+                        bounceBallImage.layout(left, top, right, bottom);
+
+                    }
+                });
+                bounceBallImage.startAnimation(transAnim);
+            }
+        });
+
+
+        //ball end//
 
         //sound start//
         // AudioManager audio settings for adjusting the volume
@@ -174,6 +232,11 @@ public class Board extends AppCompatActivity {
             return -1;
         else return 0;
     }
+
+    private int getDisplayHeight() {
+        return this.getResources().getDisplayMetrics().heightPixels;
+    }
+
 
     protected boolean check_Diagonal(char player){
         int count_Equal1 = 0,count_Equal2 = 0;
