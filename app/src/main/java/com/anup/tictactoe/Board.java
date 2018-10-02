6a0afe17ff4319b67958ea4e07aa6baf1c510849
chiachuan <wu.chiachuan@gmail.com>
2018-10-02 22:03:13 +0800

    Add firework

--- app/src/main/java/com/anup/tictactoe/Board.java
+++ app/src/main/java/com/anup/tictactoe/Board.java
@@ -56,55 +56,6 @@ public class Board extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
 
-        //ball start//
-        Button bounceBallButton = (Button) findViewById(R.id.bounceBallButton);
-        final ImageView bounceBallImage = (ImageView) findViewById(R.id.bounceBallImage);
-
-        bounceBallButton.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                bounceBallImage.clearAnimation();
-                TranslateAnimation transAnim = new TranslateAnimation(0, 0, 0,
-                        getDisplayHeight()/2);
-                transAnim.setStartOffset(500);
-                transAnim.setDuration(3000);
-                transAnim.setFillAfter(true);
-                transAnim.setInterpolator(new BounceInterpolator());
-                transAnim.setAnimationListener(new AnimationListener() {
-
-                    @Override
-                    public void onAnimationStart(Animation animation) {
-                        Log.i(TAG, "Starting button dropdown animation");
-
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-                        // TODO Auto-generated method stub
-
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        Log.i(TAG,
-                                "Ending button dropdown animation. Clearing animation and setting layout");
-                        bounceBallImage.clearAnimation();
-                        final int left = bounceBallImage.getLeft();
-                        final int top = bounceBallImage.getTop();
-                        final int right = bounceBallImage.getRight();
-                        final int bottom = bounceBallImage.getBottom();
-                        bounceBallImage.layout(left, top, right, bottom);
-
-                    }
-                });
-                bounceBallImage.startAnimation(transAnim);
-            }
-        });
-
-
-        //ball end//
-
         //sound start//
         // AudioManager audio settings for adjusting the volume
         audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
